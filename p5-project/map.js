f6c53131a413f2392ae3d133b307e068a7aec56a
dccsillag <dccsillag@gmail.com>
2019-10-30 19:56:13 -0300

    Added framework for filters

--- p5-project/map.js
+++ p5-project/map.js
@@ -1,22 +1,30 @@
-d3.csv("test.csv")
-    .then(function(data) {
-        var myMap = L.map('map').setView([0, 0], 4);
+var myMap = L.map('map').setView([0, 0], 4);
 
-        var Wikimedia = L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png', {
-            attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
-            minZoom: 1,
-            maxZoom: 19
-        }).addTo(myMap);
+var Wikimedia = L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png', {
+    attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
+    minZoom: 1,
+    maxZoom: 19
+}).addTo(myMap);
 
-        // Add the markers
-        var markers = []
-        data.forEach(entry => markers.push(L.marker([entry.latitude, entry.longitude]).addTo(myMap).bindPopup(
-                //"<img class=\"previewimg\" src=\"imgs/" + entry["file name"] + "\"><br>" +
-                "<img src=\"imgs/" + entry["file name"] + "\" width=\"400\" /><br>" +
-                "<br>" +
-                "<b>Title:</b> <i>" + entry.title + "</i><br>" +
-                "<b>Author:</b> " + entry.author + "<br>" +
-                "<b>Time of image:</b> " + entry['time of image'],
-                { maxWidth: 'auto' }
-            )));
-    });
+clustermarkers = L.markerClusterGroup();
+
+function addMarkers(data) {
+    data.forEach(entry => clustermarkers.addLayer(L.marker([entry.latitude, entry.longitude]).bindPopup(
+            "<img class=\"unselectable\" src=\"imgs/" + entry["file name"] + "\" width=\"400\" /><br class=\"unselectable\">" +
+            "<br class=\"unselectable\">" +
+            "<span class=\"unselectable\"> <b>Title:</b> </span><i>" + entry.title + "</i><br class=\"unselectable\">" +
+            "<span class=\"unselectable\"> <b>Author:</b> </span>" + entry.author + "<br class=\"unselectable\">" +
+            "<span class=\"unselectable\"> <b>Time of image:</b> </span>" + entry['time of image'] + "<br class=\"unselectable\">" +
+            "<span class=\"unselectable\"> <b>Location:</b> </span>" + entry['location string'],
+            { maxWidth: 'auto' }
+        )));
+}
+
+function mapUpdateFilters(data) {
+    clustermarkers.clearLayers();
+    addMarkers(data);
+}
+
+
+d3.csv("test.csv").then(addMarkers);
+myMap.addLayer(clustermarkers);
